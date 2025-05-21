1cf0098c3359756bac634c660da58f86da512847
Ronaldo Montan <ronaldo.montan@nappsolutions.com>
2025-05-20 21:05:12 -0300

    FEAT: menu to choose

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"ordenacaobusca/bubbleSort"
+	"ordenacaobusca/insertionSort"
+	"fmt"
 )
 func main() {
 	array :=[]int{8,5,1,6,9,5,7}
-	bubbleSort.BubbleSort(array)	
+
+	var choice int
+	fmt.Println("Choose the sorting algorithm you want to use:")
+	fmt.Println("1. Bubble Sort")
+	fmt.Println("2. Insertion Sort")
+	_, err := fmt.Scanln(&choice)
+
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a number.")
+	}
+
+	switch choice {
+		case 1:
+			bubbleSort.BubbleSort(array)
+
+		case 2:
+			insertionSort.InsertionSort(array)
+
+		default:
+			fmt.Println("Invalid choice. Please select 1 or 2.")
+	}
 }
